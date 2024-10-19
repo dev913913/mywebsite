aea6c4df1a3c32de964f98e46a2a3b2095e9a5ff
codingwithDev <165135910+dev913913@users.noreply.github.com>
2024-10-19 09:33:43 +0000

    Removing Firebase for now

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-function signIn() {
-    console.log('Sign-In button clicked!');
-    // Firebase Authentication code will go here
-}
-function signIn() {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider)
-        .then((result) => {
-            console.log('User signed in:', result.user);
-        })
-        .catch((error) => {
-            console.error('Sign-in error:', error);
-        });
-}
+// function signIn() {
+//     console.log('Sign-In button clicked!');
+//     // Firebase Authentication code will go here
+// }
+// function signIn() {
+//     var provider = new firebase.auth.GoogleAuthProvider();
+//     firebase.auth().signInWithPopup(provider)
+//         .then((result) => {
+//             console.log('User signed in:', result.user);
+//         })
+//         .catch((error) => {
+//             console.error('Sign-in error:', error);
+//         });
+// }
